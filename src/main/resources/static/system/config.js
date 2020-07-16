a1f8a099d9081fb8575ede95ab754a99d2d41e63
aihaozai <44828230+aihaozai@users.noreply.github.com>
2020-07-16 23:24:19 +0800

    Update config.js
    webscoket

--- src/main/resources/static/system/config.js
+++ src/main/resources/static/system/config.js
@@ -8,6 +8,6 @@ layui.define(function(exports) {
     mobileWidth: 992,
     webSocket: 'ws://47.115.80.180:80/websocket/socketServer',
     mozWebSocket: 'ws://47.115.80.180:80/websocket/socketServer',
-    sockJS: 'http://47.115.80.180:80/sockjs/socketServer'
+    sockJS: 'http://47.115.80.180:80/sockjs/socketServer' 
   })
 });
